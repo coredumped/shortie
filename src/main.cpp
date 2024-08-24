ab0fc202b9e8182023080eabd1d1c31b543128d5
Juan V. Guerrero <coredumped@users.noreply.github.com>
2024-08-24 17:40:04 -0500

    Allow displaying version and help

--- src/main.cpp
+++ src/main.cpp
@@ -17,6 +17,13 @@ int main(int argc, char *argv[]) {
 
 
 void help(int argc, char *argv[]) {
-  std::cerr << argv[0] << " [PATH]\n";
+  std::cout << argv[0] << " [PATH]\n";
+  std::cout << "Shortens a filesystem path by displaying only a few characters\n per path component.\n\n";
+  std::cout << " -h, --help\tDisplay this help message\n";
+  std::cout << " -v, --version\tGet version\n";
+  std::cout << " --home\t\tReplace home directory with tilde (~)\n";
+  std::cout << " --no-home\tInverse of --home\n";
+  std::cout << " -n <number>\tCount of characters to print per path component\n";
+  std::cout << "\nIf not PATH is given it default to the current working directory.\n";
   exit(EXIT_SUCCESS);
 }
